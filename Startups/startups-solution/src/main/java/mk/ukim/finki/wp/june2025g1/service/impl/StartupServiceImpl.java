739c18bdcb3b2d8b503c5db48bee90611ec15219
Jakimovski Doncho <jakimovskidonce@gmail.com>
2026-02-08 23:50:41 +0100

    Added startups task with a solution

---
+++ Startups/startups-solution/src/main/java/mk/ukim/finki/wp/june2025g1/service/impl/StartupServiceImpl.java
@@ -0,0 +1,80 @@
+package mk.ukim.finki.wp.june2025g1.service.impl;
+
+import lombok.AllArgsConstructor;
+import mk.ukim.finki.wp.june2025g1.model.Industry;
+import mk.ukim.finki.wp.june2025g1.model.Startup;
+import mk.ukim.finki.wp.june2025g1.model.exceptions.InvalidStartupIdException;
+import mk.ukim.finki.wp.june2025g1.repository.StartupRepository;
+import mk.ukim.finki.wp.june2025g1.service.FounderService;
+import mk.ukim.finki.wp.june2025g1.service.StartupService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static mk.ukim.finki.wp.june2025g1.service.specifications.FieldFilterSpecification.*;
+
+@Service
+@AllArgsConstructor
+public class StartupServiceImpl implements StartupService {
+    private final StartupRepository startupRepository;
+    private final FounderService founderService;
+
+    @Override
+    public List<Startup> listAll() {
+        return startupRepository.findAll();
+    }
+
+    @Override
+    public Startup findById(Long id) {
+        return startupRepository.findById(id).orElseThrow(InvalidStartupIdException::new);
+    }
+
+    @Override
+    public Startup create(String name, Double valuation, Integer yearFounded, Industry industry, Long founderId) {
+        return this.startupRepository.save(new Startup(name, valuation, yearFounded, industry, this.founderService.findById(founderId)));
+    }
+
+    @Override
+    public Startup update(Long id, String name, Double valuation, Integer yearFounded, Industry industry, Long founderId) {
+        Startup startup = this.findById(id);
+        startup.setName(name);
+        startup.setValuation(valuation);
+        startup.setYearFounded(yearFounded);
+        startup.setIndustry(industry);
+        startup.setFounder(founderService.findById(founderId));
+        return this.startupRepository.save(startup);
+    }
+
+    @Override
+    public Startup delete(Long id) {
+        Startup startup = this.findById(id);
+        this.startupRepository.delete(startup);
+        return startup;
+    }
+
+    @Override
+    public Startup deactivate(Long id) {
+        Startup startup = this.findById(id);
+        startup.setActive(false);
+        return this.startupRepository.save(startup);
+    }
+
+    @Override
+    public Page<Startup> findPage(String name, Double valuation, Integer yearFounded, Industry industry, Long founderId, int pageNum, int pageSize) {
+        Specification<Startup> specification = Specification.allOf(
+                filterContainsText(Startup.class, "name", name),
+                greaterThan(Startup.class, "valuation", valuation),
+                greaterThan(Startup.class, "yearFounded", yearFounded),
+                filterEqualsV(Startup.class, "industry", industry),
+                filterEquals(Startup.class, "founder.id", founderId)
+        );
+
+        return this.startupRepository.findAll(
+                specification,
+                PageRequest.of(pageNum, pageSize));
+    }
+
+}
